da20abed27b6b5d27a6b5819039311f51bbd3c3c
Proxima11 <c14210139@john.petra.ac.id>
2024-04-16 08:49:36 +0700

    all page layout

--- app/src/main/java/kris/ks/prandorid/MainActivity.kt
+++ app/src/main/java/kris/ks/prandorid/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.first_page)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.history)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
